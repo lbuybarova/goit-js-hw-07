93083a7fd25e8cab23a41c220eeb209b4061be5d
Elizaveta Buibarova <liza.buybarova@gmail.com>
2022-02-22 00:20:56 +0200

    2е задание

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+function createGalleryMarkup(images) {
+    return images.map(({preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>        
+        `;
+    })
+        .join(' '); 
+}
+
+const gallery = document.querySelector(".gallery");
+
+const galleryItemMarkup = createGalleryMarkup(galleryItems);
+gallery.innerHTML = galleryItemMarkup;
+
+const newLightbox = new SimpleLightbox('.gallery a', {    
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
