cd9edce8291628f04ea18d8d0b3975a2ecc2f6e3
Elizaveta Buibarova <liza.buybarova@gmail.com>
2023-03-28 22:38:11 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+function createGallaryCards(images) {
+    return images.map(({preview, original, description }) => {
+        return `<div class="gallery__item">
+        <a href="${original}" class="gallery__link">
+        <img
+        class="gallery__image"
+        src="${preview}" 
+        data-source="${original}"
+        alt="${description}">        
+        </a>
+        </div>`;
+    })
+        .join(' '); 
+}
+
+const cards = createGallaryCards(galleryItems);
+gallery.innerHTML = cards;
+
+let instance = {};
+
+gallery.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    const img = evt.target.dataset.source;        
+    createInstance(img);    
+})
+  
+function createInstance(img) {
+     instance = basicLightbox.create(`
+            <img src="${img}">`,
+        {
+            onShow: () => {
+                
+                document.addEventListener(('keydown'), escClose)
+            },
+            onClose: () => {
+                
+                document.removeEventListener(('keydown'), escClose)
+            }
+        });
+    
+    instance.show();
+}
+
+function escClose(evt) {
+    
+    if (evt.code === 'Escape') {
+        
+        return instance.close();        
+        }
+    }
